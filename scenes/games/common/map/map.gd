38d0f8db6bd79b8ee4e8da185259387154ce5d05
Eric <eric.espie@eboreal.com>
2023-05-24 15:23:24 +0200

    3D terrain

--- scenes/games/common/map/map.gd
+++ scenes/games/common/map/map.gd
@@ -1,32 +1,39 @@
 extends TileMap
 
+@export var ratio: float
+@export var water_height: float
+
 var moisture = FastNoiseLite.new()
 var temperature = FastNoiseLite.new()
 var altitude = FastNoiseLite.new()
 var width = 256
 var height = 256
 
-func _ready():
+
+func _ready() -> void:
 	moisture.seed = randi()
 	temperature.seed = randi()
 	altitude.seed = randi()
-	altitude.frequency = 0.005
-	generate_chunk(Vector2i(0, 0))
+	altitude.frequency = 0.05
+	generate_chunk()
 
 
-func generate_chunk(pos):
-	var tile_pos = local_to_map(pos)
-	var w2 = 0
-	var h2 = 0
+func generate_chunk() -> void:
 	for x in range(width):
 		for y in range(height):
-			var cell_pos = Vector2i(tile_pos.x-w2 + x, tile_pos.y-h2 + y)
-			if get_cell_source_id(0, cell_pos) == -1:
-				var moist = moisture.get_noise_2d(tile_pos.x-w2 + x, tile_pos.y-h2 + y)*10
-				var temp = temperature.get_noise_2d(tile_pos.x-w2 + x, tile_pos.y-h2 + y)*10
-				var alt = altitude.get_noise_2d(tile_pos.x-w2 + x, tile_pos.y-h2 + y)*10
+			var cell_pos = Vector2i(x, y)
 			
-				if alt < -5:
-					set_cell(0, cell_pos, 0, Vector2(3, round((temp+10)/5)))
-				else:
-					set_cell(0, cell_pos, 0, Vector2(round((moist+10)/5), round((temp+10)/5)))
+			var noise_x = x * ratio
+			var noise_y = y * ratio
+			
+			var moist = moisture.get_noise_2d(noise_x, noise_y)
+			var temp = temperature.get_noise_2d(noise_x, noise_y)
+			var alt = altitude.get_noise_2d(noise_x, noise_y)
+		
+			var iy = round(temp * 2 + 2)
+			var ix = round(moist * 2 + 2)
+			
+			if alt < water_height:
+				set_cell(0, cell_pos, 0, Vector2i(3, 3))
+			else:
+				set_cell(0, cell_pos, 0, Vector2i(ix, iy))
