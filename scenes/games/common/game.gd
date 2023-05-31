6622f01167516bd6224c735d75b3c16f1dcaec9b
Eric <eric.espie@eboreal.com>
2023-06-01 00:19:47 +0200

    textured map

--- scenes/games/common/game.gd
+++ scenes/games/common/game.gd
@@ -4,6 +4,8 @@ extends Node2D
 	get:
 		return game_name
 
+var mouse = Vector2()
+
 func _ready():
 	Events.trigger("new_game", game_name)
 	
@@ -13,8 +15,17 @@ func _process(_delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_released("pause"):
 		call_deferred("_pause")
-		
+	if event is InputEventMouse:
+		mouse = event.position
+	if event is InputEventMouseButton and event.is_pressed():
+		get_color()
+				
 func _pause() -> void:
 	$GUI/Paused.pause()
 	get_tree().paused = true
 
+
+func get_color():
+	var terrain = get_tree().get_first_node_in_group("terrain")
+	
+	
--- scenes/games/common/terrain/terrain_gen.gd
+++ scenes/games/common/terrain/terrain_gen.gd
@@ -1,4 +1,3 @@
-@tool
 extends MeshInstance3D
 
 @export_category("Mesh Settings")
